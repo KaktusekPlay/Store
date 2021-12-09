3c825f30653a839898d1a820b8de8b899229ea35
Jozo_85 <pepinoprochajda2@seznam.cz>
2021-12-09 14:45:49 +0100

    add

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-zooykp
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-zooykp)
---
+++ src/app/products.ts
@@ -0,0 +1,33 @@
+export interface Product {
+  id: number;
+  name: string;
+  price: number;
+  description: string;
+}
+
+export const products = [
+  {
+    id: 1,
+    name: 'Iron VIP',
+    price: 799,
+    description: 'Iron vip na kaktusku play!!',
+  },
+  {
+    id: 2,
+    name: 'Gold VIP',
+    price: 699,
+    description: 'Gold VIP na kaktusku play!!',
+  },
+  {
+    id: 3,
+    name: 'Diamond VIP',
+    price: 299,
+    description: 'Diamond VIP na kaktusku play',
+  },
+];
+
+/*
+Copyright Google LLC. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at https://angular.io/license
+*/
